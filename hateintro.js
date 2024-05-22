f82f8fa68fa73ab2da4c17471c9d59bdb633b7c4
Anahat07 <anahatkchhatwal@gmail.com>
2024-05-22 14:30:18 -0400

    initial commits

--- hateintro.js
+++ hateintro.js
@@ -7,19 +7,3 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-  
-  /*Image Slider*/
-  document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-      loop: true,
-      slidesPerView: 1,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  }); 
